06f4bd438d35836cf5fce39ef95ffca6beb5d476
unajbekov <i.unaybekov@zenit.ru>
2022-03-25 12:23:53 +0300

    2

--- js/script.js
+++ js/script.js
@@ -45,8 +45,6 @@ $( () => {
       variableWidth: true
     });
 
-
-      
     // smooth scroll
     $('a.nav-menu-link').click(function(){
         var _href = $(this).attr('href')
@@ -71,46 +69,6 @@ $( () => {
       highlight: true,
       nodata: 'Результаты не найдены',
       ignore_accents: true,
-  });
-});
-
-//accordeon
-const accOpen = document.querySelectorAll('.doc'); 
-    accOpen.forEach(doc => {
-        doc.addEventListener('click', () => {
-            doc.classList.toggle('active');
-        });
     });
 
-// //mobile menu
-// const menuButton = document.querySelector('.menu-button');
-// const menu = document.querySelector('.nav-menu');
-// const menuClose = document.querySelector('.menu-close');
-// const linksClose = document.querySelectorAll('.nav-menu-link');
-// const links = document.querySelectorAll('.nav-menu-link');
-
-// menuButton.addEventListener('click', () => {
-//     menu.classList.add('is-active');
-//     menuClose.classList.add('is-active');
-// });
-// menuClose.addEventListener('click', () => {
-//     menu.classList.remove('is-active');
-//     menuClose.classList.remove('is-active');
-// });
-// linksClose.forEach(linkClose => {
-//     linkClose.addEventListener('click', () => {
-//         menu.classList.remove('is-active');
-//         menuClose.classList.remove('is-active');
-//     });
-// });
-
-// добавление класса при наведении
-
-// $('#main').hover(
-//     function () {
-//       $('#projects').addClass('megaMenu-active');
-//     },
-//     function () {
-//       $('#projects').removeClass('megaMenu-active');
-//     }
-// );
+});
