5ce7916ee5f78e07f810822d01e3adefd04cf7c4
unajbekov <i.unaybekov@zenit.ru>
2022-03-30 01:53:34 +0300

    1

--- js/script.js
+++ js/script.js
@@ -38,11 +38,12 @@ $( () => {
 
     $('.variable-width').slick({
       dots: true,
+      arrows: true,
       infinite: true,
       speed: 300,
       slidesToShow: 1,
-      centerMode: true,
-      variableWidth: true
+      centerMode: false,
+      variableWidth: true,
     });
 
     // smooth scroll
